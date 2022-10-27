2c95e3a99500cf64f2c9e048f4e59fabe6832b36
youssefkab <ykablaoui.123@gmail.com>
2022-10-27 17:32:48 +0200

    initiale commit

---
+++ inscription/inscription.js
@@ -0,0 +1,7 @@
+function controle(){
+    var saisie =document.getElementsByClassName("nom").textcontent;
+    var saisiee =document.getElementsByClassName("prenom").innertext;
+    var checked=document.getElementsByClassName("check").checked;
+    alert("Votre nom est : "+ saisie + ". Votre prenom est : "+ saisiee + ". Vos interet sont: "+checked);
+
+    }
